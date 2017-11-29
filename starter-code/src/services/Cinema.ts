e0baf91fa13b7add3b9e2491302e137a9cf0fe90
Riftland <fran.mscr@gmail.com>
2017-11-29 17:57:22 +0100

    1st iteration done

---
+++ starter-code/src/services/Cinema.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { IMovies } from '../interfaces/IMovies';
+
+@Injectable()
+export class Cinema implements IMovies{
+
+  //Properties from interface
+  id:number;
+  title:string;
+  poster:string;
+  synopsis:string;
+  genres:Array<string>;
+  year:number;
+  director:string;
+  actors:Array<string>;
+  hours:Array<string>;
+  room:number;
+
+  movies:Array<IMovies> = [];
+
+  getMovies(){
+    return this.movies;
+  }
+
+  getMovie(id:number){
+    return this.movies[id];
+  }
+}
